6ce842d2f5becbda95896deb42587d4d47487f22
esoshyki <esoshyki@yandex.by>
2020-12-10 21:03:10 +0300

    add markers on map

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,10 +1,16 @@
 import styles from './Map.module.sass';
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+import { useEffect, useState } from 'react';
+import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps"
+import Coords from './Coords'
 
 const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
 
-export default function Map () {
+export default function Map ({countries}) {
+
+  const _countries = countries.map(el => ({
+    ...el,
+    coords: Coords[el.CountryCode.toLowerCase()]}))
 
   return (
     <div className={styles.root}>
@@ -14,6 +20,12 @@ export default function Map () {
           geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} />)
         }
       </Geographies>
+      {_countries.map((con, idx) => {
+      const lat = con.coords?.lat;
+      const long = con.coords?.long;
+      return <Marker key={idx} coordinates={[long, lat]}>
+        <circle r={8} fill="#F53" />
+      </Marker>})}
     </ComposableMap>
     </div>
   )
